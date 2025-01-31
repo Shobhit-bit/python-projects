654ebccdfe29d827dfd824c449ccc68c92d0a126
Shobhit-bit <shobhitsahu1302@gmail.com>
2025-01-31 20:38:03 +0530

    Update dsajourney.py

--- project 2024/dsajourney.py
+++ project 2024/dsajourney.py
@@ -191,3 +191,42 @@ def revstring(a):
     return revstr
 
 print(revstring("alu ke parathe kha ke needh aayi"))
+
+# Write a function in python that checks if paranthesis in the string are balanced or not. Possible parantheses are "{}',"()" or "[]". Use Stack class from the tutorial
+# nhi aaya
+
+# Design a food ordering system where your python program will run two threads,
+# Place Order: This thread will be placing an order and inserting that into a queue. This thread places new order every 0.5 second. (hint: use time.sleep(0.5) function)
+# Serve Order: This thread will server the order. All you need to do is pop the order out of the queue and print it. This thread serves an order every 2 seconds. Also start this thread 1 second after place order thread is started.
+# Pass following list as an argument to place order thread
+# orders = ['pizza','samosa','pasta','biryani','burger']
+class queue:
+    def __init__(self):
+        self.buffer = deque()
+    def endeque(self,value):
+        self.buffer.appendleft(value)
+    def deqque(self):
+        return(self.buffer.pop())
+    def is_empty(self):
+        return(len(self.buffer)==0)
+    def lenque(self):
+        return(len(self.buffer))
+    def orpLA(self,arr):
+        for val in arr:
+            print(f"in {val}")
+            self.endeque(val)
+            time.sleep(0.5)
+    def orserverd(self):
+        time.sleep(1)
+        while True:
+            print(f"out {self.deqque()}")
+            time.sleep(2)
+queque = queue()
+orders = ['pizza','samosa','pasta','biryani','burger']
+t1 = threading.Thread(target=queque.orpLA,args=(orders,))
+t2 = threading.Thread(target = queque.orserverd)
+t1.start()
+t2.start()
+t1.join()
+time.sleep(1)
+t2.join()
